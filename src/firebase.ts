5ed41453c4149d8560f51a1ad6554111c5c7b5b1
jiraporn404 <chm.jiraporn@gmail.com>
2025-07-10 13:45:05 +0700

    Refactor Firebase configuration.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA9TTO3PX1BDBcgQPzkDL4xzw_2wUJvSi0",
-  authDomain: "game24-d79c9.firebaseapp.com",
-  projectId: "game24-d79c9",
-  storageBucket: "game24-d79c9.firebasestorage.app",
-  messagingSenderId: "866955282884",
-  appId: "1:866955282884:web:7fecb1af259e3f835c7c91",
-  measurementId: "G-7798X58DCX",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
